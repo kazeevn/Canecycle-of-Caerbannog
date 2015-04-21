fed98234d0e8c7a4fe3f695e694d3976d9683298
Nikita Kazeev <kazeevn@yandex-team.ru>
2015-04-21 16:17:06 +0300

    Fixed hash_size

--- python/hash_function.py
+++ python/hash_function.py
@@ -5,8 +5,8 @@ class HashFunction:
     def __init__(hash_size):
         if hash_size > 63:
             raise ValueError(
-                "We appreciate your hardware, but the maximum supported hash size is 61")
-        self.hash_size = hash_size
+                "We appreciate your hardware, but the maximum supported hash size is 63")
+        self.hash_size = 2**hash_size
     
     def hash(string):
         return spooky.hash64(string) % self.hash_size
