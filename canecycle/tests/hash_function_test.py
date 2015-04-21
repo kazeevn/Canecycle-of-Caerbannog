9dafe31b644e9be4829b332cf5fb3fe79e07e449
Nikita Kazeev <kazeevn@yandex-team.ru>
2015-04-21 17:00:27 +0300

    HashFunction tests run.

--- canecycle/tests/hash_function_test.py
+++ canecycle/tests/hash_function_test.py
@@ -1,12 +1,12 @@
 import unittest
 import numpy
 
-import canecycle.hash_function
+from canecycle.hash_function import HashFunction
 
 class TestHashFunction(unittest.TestCase):
 
     def test_uncut_points(self):
-        hash_ = hash_function.HashFunction(63)
+        hash_ = HashFunction(63)
         self.assertEqual(hash_.hash("Kurchatov"), 6304225726192864752L)
         self.assertEqual(hash_.hash("Landau"), 3567067149801099079L)
         
@@ -14,7 +14,7 @@ class TestHashFunction(unittest.TestCase):
     def test_range(self):
         numpy.random.seed(42)
         for hash_size in xrange(10):
-            hash_ = hash_function.HashFunction(hash_size)
+            hash_ = HashFunction(hash_size)
             for value in map(str, numpy.random.randint(1775, size=100)):
-                self.assetLess(hash_.hash(value), 2**hash_size)
+                self.assertLess(hash_.hash(value), 2**hash_size)
         
