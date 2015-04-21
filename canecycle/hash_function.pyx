16aedf3bb2f31823697b4cf0d980398ce0ec4756
graring <shiryaev.petr@gmail.com>
2015-04-21 18:36:36 +0400

    Negative hash size case

--- canecycle/hash_function.pyx
+++ canecycle/hash_function.pyx
@@ -1,5 +1,5 @@
 import spooky
-import ctypes 
+import ctypes
 
 MAX_HASH_SIZE = 63
 
@@ -8,10 +8,11 @@ cdef class HashFunction:
     def __init__(self, hash_size):
         if hash_size > MAX_HASH_SIZE:
             raise ValueError(
-                "We appreciate your hardware, but the maximum supported hash size is %d" %
-		MAX_HASH_SIZE)
+                "We appreciate your hardware, but the maximum supported hash size is %d." %
+                MAX_HASH_SIZE)
+        if hash_size < 0:
+            raise ValueError("Hash size should be positive.")
         self.hash_size = 2**hash_size
     
     cpdef unsigned long hash(self, string):
         return spooky.hash64(string) % self.hash_size
-
