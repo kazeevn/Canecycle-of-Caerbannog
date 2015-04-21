5d4020dcf8285bbc7979f5f17286f186f7ac386b
dsmolyakov <d.s.smolyakov@gmail.com>
2015-04-21 16:14:50 +0300

    setup.py

--- python/hash_function.py
+++ python/hash_function.py
@@ -1,13 +1,16 @@
 import spooky
 import ctypes 
 
+MAX_HASH_SIZE = 63
+
+
 class HashFunction:
-    def __init__(hash_size):
-        if hash_size > 63:
+    def __init__(self, hash_size):
+        if hash_size > MAX_HASH_SIZE:
             raise ValueError(
                 "We appreciate your hardware, but the maximum supported hash size is 61")
         self.hash_size = hash_size
     
-    def hash(string):
+    def hash(self, string):
         return spooky.hash64(string) % self.hash_size
 
