a5cb30b6a261a728ab89ba11022117d4b00a9833
graring <shiryaev.petr@gmail.com>
2015-04-22 22:00:13 +0300

    Item: public members

--- canecycle/item.pyx
+++ canecycle/item.pyx
@@ -1,12 +1,13 @@
 import ctypes
-from scipy.sparse import csr_matrix
+from scipy.sparse import coo_matrix
 
-cdef class Item:
-    cdef int label
-    cdef double weight
+cdef class Item(object):
+    cpdef public int label
+    cpdef public double weight
+    cpdef public features
     
     def __cinit__(self, unsigned int features_number):
         self.label = 0
         self.weight = 1.
-        self.features = csr_matrix(features_number, dtype=float)
+        self.features = coo_matrix(features_number, dtype=float)
     
