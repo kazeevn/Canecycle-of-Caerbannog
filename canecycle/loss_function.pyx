c1094f1c5287c0169a056f53d68ceedffc8c4602
d.s.smolyakov <d.s.smolyakov@gmail.com>
2015-04-23 21:38:36 +0300

    Logistic Loss Function

---
+++ canecycle/loss_function.pyx
@@ -0,0 +1,32 @@
+#cython: boundscheck=False
+#cython: wraparound=False
+#cython: cdivision=True
+#cython: nonecheck=False
+
+
+from canecycle.item cimport Item
+import numpy as np
+cimport numpy as np
+
+
+ctypedef np.double_t cDOUBLE
+ctypedef np.int32_t cINT32
+
+
+cdef class LossFunction(object):
+    cdef cDOUBLE get_loss(self, Item item, 
+                                 np.ndarray[cDOUBLE, ndim=1] weights):
+        cdef np.ndarray[cDOUBLE, ndim=1] features = item.features
+        return 1. / (1. + np.exp(-np.dot(weights, features)))
+
+    cdef get_decision(self, Item item, np.ndarray[cDOUBLE, ndim=1] weights):
+        cdef cDOUBLE loss = self.get_loss(item, weights)
+        return loss > 0.5
+
+    cdef np.ndarray[cDOUBLE, ndim=1] get_gradient(self, 
+                                        np.ndarray[cDOUBLE, ndim=1] weights, Item item):
+        cdef cDOUBLE sigmoid_values = self.get_loss(item, weights)
+        cdef cINT32 label = item.label
+        cdef np.ndarray[cDOUBLE, ndim=1] features = item.features
+        return (label - self.get_loss(item, weights)) * features
+
