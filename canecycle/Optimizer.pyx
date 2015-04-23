5cb182fc508fb58a142268f5bcc47d9deac27904
dsmolyakov <d.s.smolyakov@gmail.com>
2015-04-23 20:15:05 +0300

    Optimizer

---
+++ canecycle/Optimizer.pyx
@@ -0,0 +1,53 @@
+#cython: boundscheck=False
+#cython: wraparound=False
+#cython: cdivision=True
+#cython: nonecheck=False
+
+import numpy as np
+cimport numpy as np
+from item import Item
+
+from scipy.sparse import coo_matrix
+
+ctypedef np.int32_t cINT32
+ctypedef np.double_t cDOUBLE
+
+
+cdef class Optimizer(object):
+
+    cdef double l1Regularization
+    cdef double l2Regularization
+    cdef double stepSize
+    cdef double scaleDown
+    cdef loss_function
+
+    def __init__(self, l1Regularization, l2Regularization, stepSize, scaleDown, loss_function):
+        self.l1Regularization = l1Regularization
+        self.l2Regularization = l2Regularization
+        self.stepSize = stepSize
+        self.scaleDown = scaleDown
+        self.loss_function = loss_function
+
+    def step(self, item, unsigned int step_number, weights):
+        cdef int index, element_index, label
+        cdef double step_size
+        cdef np.ndarray[cINT32, ndim=1] col
+        cdef np.ndarray[cDOUBLE, ndim=1] data
+        features = item.features
+        label = item.label
+        step_size = self.stepSize * self.scaleDown ** step_number
+        gradient = self.loss_function.get_gradient(features, label)
+        newStep = gradient * step_size
+        col = newStep.col
+        data = newStep.data
+        for index in range(np.shape(data)[0]):
+            candidate_weight = weights[col[index]] - newStep.data[index]
+            if candidate_weight > 0:
+                candidate_step = candidate_weight - self.l1Regularization * gradient.data[index]
+                weights[col[index]] = max(0.0, candidate_step)
+            else:
+                candidate_step = candidate_weight + self.l1Regularization * gradient.data[index]
+                weights[col[index]] = min(0.0, candidate_step)
+        return weights
+
+
---
+++ canecycle/tests/OptimizerTest.py
@@ -0,0 +1,26 @@
+import unittest
+import numpy as np
+
+from scipy.sparse import coo_matrix
+from ..Optimizer import Optimizer
+from ..item import Item
+
+class LossFunction(object):
+
+    def get_gradient(self, point, label):
+        return point * 2
+
+
+class OptimizerTestCase(unittest.TestCase):
+    def test_zero_vector(self):
+        point = coo_matrix([0., 0., 0.])
+        loss_function = LossFunction()
+        item = Item(point, 0)
+        optimizer = Optimizer(l1Regularization=0, l2Regularization=0,
+                              stepSize=0.1, scaleDown=0.9, loss_function=loss_function)
+        test_value = np.random.rand(3)
+        point = optimizer.step(item, 1, test_value)
+        np.testing.assert_array_almost_equal(point, test_value)
+
+if __name__ == '__main__':
+    unittest.main()
