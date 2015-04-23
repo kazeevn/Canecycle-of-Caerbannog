08120e7697ee50cc89a97f49f928b39874aaa037
graring <shiryaev.petr@gmail.com>
2015-04-23 11:24:43 +0300

    Fixed classifier

--- canecycle/classifier.pyx
+++ canecycle/classifier.pyx
@@ -1,107 +1,123 @@
 from __future__ import division
 
-from scipy.sparse import coo_matrix
 import numpy as np
 cimport numpy as np
 
-from canecycle.item import Item
-from canecycle.reader import Reader
+from canecycle.item cimport Item
+from canecycle.reader cimport Reader
+#TODO(shiryaev) import->cimport
 from canecycle.weight_manager import WeightManager
 from canecycle.optimizer import Optimizer
 from canecycle.loss_function import LossFunction
 
-
+#TODO(shiryaev) save/load
 cdef class Classifier(object):
-    cdef float average_loss
-    cdef float holdout_loss
-    cdef int holdout_items
     cdef weight_manager
     cdef optimizer
     cdef loss_function
-    cdef list progressive_validation_loss
+    
+    cdef get_progressive_validation
+    cdef unsigned int pass_number
+    cdef unsigned long items_processed
+    cdef unsigned long validation_index
+    cdef np.ndarray weights
+    
     cdef int holdout
-    cdef int pass_number
-    cdef int items_processed
-    cdef weights
+    cdef unsigned long holdout_items_passed
+    cdef list progressive_validation_loss # optimizer loss
+    cdef float average_training_loss # optimizer loss
+    cdef float holdout_loss # lossfunc loss
+    
     cdef display
     
     def __cinit__(self, optimizer, loss_function, weight_manager,
-            int get_progressive_validation, int holdout, int pass_number,
+            get_progressive_validation, int holdout, int pass_number,
             display=False):
-        
-        if holdout < 0:
-            raise ValueError("Negative holdout.")
         
         if pass_number < 0:
             raise ValueError("Negative number of passes.")
         
         self.optimizer = optimizer
         self.loss_function = loss_function
+        self.weight_manager = weight_manager
         self.get_progressive_validation = get_progressive_validation
         self.holdout = holdout
         self.pass_number = pass_number
-        self.weight_manager = weight_manager
         self.display = display
     
+    cdef int predict_item(self, Item item):
+        return self.loss_function.get_decision(item, self.weights)
     
-    cdef int predict_item(self, item):
-        return self.optimizer.get_decision(item, self.weights)
+    cdef float predict_proba_item(self, Item item):
+        return self.loss_function.get_loss(item, self.weights)
     
-    
-    cdef float predict_proba_item(self, item):
-        return self.optimizer.get_loss(item, self.weights)
-    
-    cdef void predict(self, reader, output_file):
-        #TODO: display
+    def predict(self, Reader reader):
+        #TODO(shiryaev): display
+        cdef Item item
+        reader.restart(0)
         for item in reader:
-            output_file.write(self.predict_item(item))
-            output_file.write('\n')
+            yield self.predict_item(item)
     
-    
-    cdef void predict_proba(self, reader, output_file):
-        #TODO: display
+    def predict_proba(self, Reader reader):
+        #TODO(shiryaev): display
+        cdef Item item
         for item in reader:
-            output_file.write(self.predict_proba_item(item))
-            output_file.write('\n')
+            yield self.predict_proba_item(item)
     
+    cdef void run_holdout_pass(self, Reader reader):
+        cdef Item item
+        for item in reader:
+            #TODO(shiryaev): display progress
+            self.holdout_loss += self.predict_proba_item(item)
+            self.holdout_items_processed += 1
     
-    cdef void fit(self, reader):
-        self.weights = coo_matrix(reader.hash_size(), dtype=np.float32)
-        self.items_processed = 0
-        self.holdout_items = 0
-        cdef unsigned int current_pass
-        cdef item = Item(reader.hash_size())
-        cdef int validation_index = 1
-        for current_pass in range(self.pass_number):
-            for item in reader:
-                
-                item.weight = self.weight_manager.get_weight(item.label, item.weight)
-                
-                #validation routine
-                if self.items_processed==validation_index-1:
-                    validation_index *= 2
-                    self.progressive_validation_loss.append(self.predict_item(item))
-                    if self.display:
-                        #TODO: display everything
-                        print self.items_processed
-                
-                if self.holdout != 0 and self.items_processed%self.holdout == 0:
-                    #holdout routine
-                    self.holdout_loss += self.predict_item(item)
-                    self.holdout_items += 1
-                else:
-                    # step routine
-                    self.optimizer.step(item, self.weights)
-                
-                self.items_processed += 1
-            reader.restart()
+    cdef void run_train_pass(self, Reader reader):
+        cdef Item item
+        for item in reader:
+            #validation routine
+            if self.items_processed==self.validation_index - 1:
+                self.validation_index *= 2
+                if self.get_progressive_validation:
+                    self.progressive_validation_loss.append(
+                        self.predict_item(item))
+                if self.display:
+                    #TODO(shiryaev): display progress
+                    print self.items_processed,
+                    print self.average_training_loss / self.items_processed
+            
+            item.weight = self.weight_manager.get_weight(item.label, item.weight)
+            self.average_loss += self.optimizer.step(item, self.weights)
+            
+            self.items_processed += 1
     
+    cdef void fit(self, Reader reader, continue_fitting=False):
+        if not continue_fitting:
+            self.weights = np.ndarray((reader.get_features_count(), 1), dtype=float)    
+            self.items_processed = 0
+            self.holdout_items = 0
+            self.validation_index = 1
+            self.average_training_loss = 0.
+            self.holdout_loss = 0.
+        
+        cdef unsigned int pass_index
+        for pass_index in range(self.pass_number):
+            reader.restart(self.holdout)
+            self.run_train_pass(reader)
+        
+        self.average_training_loss /= self.items_processed
+        self.holdout_loss /= self.holdout_items_processed
+        
+        #holdout pass
+        if self.holdout != 0:
+            reader.restart(-self.holdout)
+            self.run_holdout_pass(reader)
     
-    #TODO
     def get_progressive_validation(self):
         return self.progressive_validation_loss
     
+    def get_average_loss(self):
+        return self.average_training_loss
     
-    def hold_out(self):
-        return self.holdout_loss / self.holdout_items
+    def get_holdout_loss(self):
+        return self.holdout_loss
 
