18ed6a5749b674567cd8d19624cb226e6b9984e9
Nikita Kazeev <kazeevn@yandex-team.ru>
2015-04-27 17:46:00 +0300

    Removed scipy from dependencies.

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 import numpy
-import scipy
 
 setup(
     ext_modules = cythonize("canecycle/*.pyx"),
-    include_dirs=[numpy.get_include(), scipy.get_include()],
+    include_dirs=[numpy.get_include()],
     name='canecycle',
     url='https://github.com/kazeevn/Canecycle-of-Caerbannog',
     license='GPLv3',
