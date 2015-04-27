9d8d12adfd131e711344372f9e1b5a877f6ffe2d
dsmolyakov <d.s.smolyakov@gmail.com>
2015-04-27 18:45:29 +0300

    New Loss function

--- canecycle/loss_function.pyx
+++ canecycle/loss_function.pyx
@@ -11,26 +11,35 @@ cimport numpy as np
 
 
 cdef class LossFunction(object):
-    cdef cDOUBLE get_loss(self, Item item, 
+
+    cdef cDOUBLE get_proba(self, Item item, 
                                  np.ndarray[cDOUBLE, ndim=1] weights):
-        cdef np.ndarray[cINT32, ndim=1] features_indices = item.features.col
-        cdef np.ndarray[cDOUBLE, ndim=1] features_values = item.features.data
+        cdef np.ndarray[cINT32, ndim=1] features_indices = item.indexes
+        cdef np.ndarray[cDOUBLE, ndim=1] features_values = item.data
         cdef cDOUBLE feature_value, dot_product
-        cdef cINT32 index
+        cdef int index
         
         dot_product = 0.0
         for index, feature_value in enumerate(features_values):
             dot_product += feature_value * weights[features_indices[index]]
-
         return 1. / (1. + np.exp(-dot_product))
+
 
     cdef int get_decision(self, Item item, np.ndarray[cDOUBLE, ndim=1] weights):
         cdef cDOUBLE loss = self.get_loss(item, weights)
         return loss > 0.5
 
-    cdef get_gradient(self, np.ndarray[cDOUBLE, ndim=1] weights, Item item):
+    cdef cDOUBLE get_loss(self, Item item,
+                                   np.ndarray[cDOUBLE, ndim=1] weights):
+        cdef cINT32 label = int(item.label)
+        cdef cDOUBLE prediction = self.get_proba(item, weights)
+        cdef cDOUBLE loss = -np.log(prediction) if label == 1 else -np.log(1. - prediction)
+        return loss
+
+    cdef object get_gradient(self, np.ndarray[cDOUBLE, ndim=1] weights, Item item):
         cdef cDOUBLE sigmoid_values = self.get_loss(item, weights)
         cdef cDOUBLE label = float(item.label)
-        return (label - sigmoid_values) * item.features
+        return -(sigmoid_values - label) * item.data
+
 
 
