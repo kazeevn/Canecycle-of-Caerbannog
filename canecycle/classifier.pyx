31ba662a437bdaf1faac491a39007a5294fea173
graring <shiryaev.petr@gmail.com>
2015-04-28 15:22:15 +0400

    Classifier progress display

--- canecycle/classifier.pyx
+++ canecycle/classifier.pyx
@@ -6,17 +6,18 @@
 
 import numpy as np
 cimport numpy as np
-#TODO(shiryaev): same c-types in all project files
 from cpython cimport bool as c_bool
 
 from canecycle.item cimport Item
 from canecycle.reader cimport Reader
 from canecycle.weight_manager cimport WeightManager
-#TODO(shiryaev) import->cimport?
-from canecycle.optimizer import Optimizer
 from canecycle.loss_function cimport LossFunction
 
+#TODO(shiryaev) import->cimport
+from canecycle.optimizer import Optimizer
+
 #TODO(shiryaev) save/load model
+#TODO(shiryaev) more display information
 cdef class Classifier(object):
     cdef WeightManager weight_manager
     cdef optimizer
@@ -25,18 +26,19 @@ cdef class Classifier(object):
     cdef c_bool store_progressive_validation
     cdef np.uint64_t pass_number
     cdef np.uint64_t items_processed
-    cdef np.uint64_t validation_index
+    cdef np.uint64_t training_validation_index
     cdef np.ndarray weights
     
     cdef np.int_t holdout
     cdef np.uint64_t holdout_items_processed
+    cdef np.uint64_t holdout_validation_index
     cdef list progressive_validation_loss # optimizer loss
     cdef np.float_t average_training_loss # optimizer loss
     cdef np.float_t holdout_loss # lossfunc loss
     
     cdef c_bool display
     
-    def __cinit__(self, optimizer, loss_function, WeightManager weight_manager,
+    def __cinit__(self, optimizer, LossFunction loss_function, WeightManager weight_manager,
             c_bool store_progressive_validation, np.int_t holdout, np.uint64_t pass_number,
             c_bool display=False):
         
@@ -58,7 +60,6 @@ cdef class Classifier(object):
         return self.loss_function.get_loss(item, self.weights)
     
     def predict(self, Reader reader):
-        #TODO(shiryaev): display
         cdef Item item
         reader.restart(0)
         for item in reader:
@@ -73,7 +74,12 @@ cdef class Classifier(object):
     cdef void run_holdout_pass(self, Reader reader) except *:
         cdef Item item
         for item in reader:
-            #TODO(shiryaev): display progress
+            if self.holdout_items_processed == self.holdout_validation_index - 1:
+                self.training_validation_index *= 2
+                print '{}\t{:.6}\t{:.6}'.format(
+                    self.holdout_items_processed,
+                    self.holdout_loss / self.holdout_items_processed,
+                    self.predict_proba_item(item))
             self.holdout_loss += self.predict_proba_item(item)
             self.holdout_items_processed += 1
     
@@ -81,27 +87,33 @@ cdef class Classifier(object):
         cdef Item item
         for item in reader:
             # validation routine
-            if self.items_processed == self.validation_index - 1:
-                self.validation_index *= 2
+            if self.items_processed == self.training_validation_index - 1:
+                self.training_validation_index *= 2
                 if self.store_progressive_validation:
                     self.progressive_validation_loss.append(
                         self.predict_proba_item(item))
                 if self.display:
-                    #TODO(shiryaev): display progress
-                    print self.items_processed,
-                    print self.average_training_loss / self.items_processed
+                    print '{}\t{:.6}\t{:.6}'.format(
+                        self.items_processed,
+                        self.average_training_loss / self.items_processed,
+                        self.predict_proba_item(item))
             
             item.weight = self.weight_manager.get_weight(item.label, item.weight)
             self.average_training_loss += self.predict_proba_item(item)
             self.weights = self.optimizer.step(item, self.items_processed, self.weights)
             self.items_processed += 1
     
-    cpdef fit(self, Reader reader, continue_fitting=False):
+    cpdef fit(self, Reader reader, c_bool continue_fitting=False):
+        if self.display:
+            print '{:-^50}'.format(' TRAINING ')
+            print '{} \t {} \t {}'.format('iteration', 'average', 'last')
+            print '{}\t{}\t{}'.format('number', 'loss', 'loss')
         if not continue_fitting:
             self.weights = np.ones(reader.get_features_count(), dtype=np.float_)     
             self.items_processed = 0
             self.holdout_items_processed = 0
-            self.validation_index = 1
+            self.training_validation_index = 1
+            self.holdout_validation_index = 1
             self.average_training_loss = 0.
             self.holdout_loss = 0.
         
@@ -114,6 +126,8 @@ cdef class Classifier(object):
         
         # holdout pass
         if self.holdout != 0:
+            if self.display:
+                print '{:-^50}'.format(' HOLDOUT PASS ')
             reader.restart(-self.holdout)
             self.run_holdout_pass(reader)
             self.holdout_loss /= self.holdout_items_processed
@@ -121,9 +135,9 @@ cdef class Classifier(object):
     cpdef list get_progressive_validation(self):
         return self.progressive_validation_loss
     
-    cpdef double get_average_loss(self):
+    cpdef np.float_t get_average_loss(self):
         return self.average_training_loss
     
-    cpdef double get_holdout_loss(self):
+    cpdef np.float_t get_holdout_loss(self):
         return self.holdout_loss
 
