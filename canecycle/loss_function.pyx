13490e4c92df4d817510fdddbad0550051b27225
Nikita Kazeev <kazeevn@yandex-team.ru>
2015-05-12 00:32:34 +0300

    Removed overhead from LossFunciton.get_loss

--- canecycle/loss_function.pyx
+++ canecycle/loss_function.pyx
@@ -16,11 +16,8 @@ cdef class LossFunction(object):
                                    np.ndarray[np.float_t, ndim=1] weights):
         cdef np.float_t dot_product = np.dot(
             item.data, weights[item.indexes])
-        # http://lingpipe-blog.com/2009/06/25/log-sum-of-exponentials/
-        if dot_product < 0:
-            return dot_product - np.logaddexp(0., dot_product)
-        else:
-            return -np.logaddexp(0., -dot_product)
+        return -np.logaddexp(0., -dot_product)
+            
 
     cpdef np.float_t get_log_one_minus_proba(
         self, Item item,
@@ -28,11 +25,7 @@ cdef class LossFunction(object):
             
         cdef np.float_t dot_product = np.dot(
             item.data, weights[item.indexes])
-        # http://lingpipe-blog.com/2009/06/25/log-sum-of-exponentials/
-        if dot_product > 0:
-            return -dot_product - np.logaddexp(0, -dot_product)
-        else:
-            return -np.logaddexp(0.,dot_product)
+        return -np.logaddexp(0., dot_product)
 
 
     cpdef np.float_t get_proba(self, Item item, 
@@ -49,17 +42,19 @@ cdef class LossFunction(object):
 
 
     cpdef np.float_t get_loss(self, Item item,
-                                   np.ndarray[np.float_t, ndim=1] weights):
+                              np.ndarray[np.float_t, ndim=1] weights):
+        cdef np.float_t dot_product = np.dot(
+            item.data, weights[item.indexes])
+
         if item.label == 1:
-            return -self.get_log_proba(item, weights)
+            return np.logaddexp(0., -dot_product)
         else:
-            return -self.get_log_one_minus_proba(item, weights)
+            return np.logaddexp(0., dot_product)
 
     cpdef np.ndarray[np.float_t, ndim=1] get_gradient(self, Item item,
          np.ndarray[np.float_t, ndim=1] weights):
         cdef np.float_t sigmoid_values = self.get_loss(item, weights)
-        cdef np.float_t label = float(item.label)
-        return (sigmoid_values - label) * item.data
+        return (sigmoid_values - item.label) * item.data
     
     def __reduce__(self):
         return (LossFunction, (), {})
