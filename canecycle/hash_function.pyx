877d6c11a172afae64c15f90a639a94869adb661
graring <shiryaev.petr@gmail.com>
2015-05-12 22:53:55 +0400

    hash function documentation

--- canecycle/hash_function.pyx
+++ canecycle/hash_function.pyx
@@ -6,7 +6,13 @@ cdef np.uint64_t MAX_HASH_SIZE = 2**63
 
 
 cdef class HashFunction(object):
+    """Class to calculate hash of string.
+    Attributes:
+        hash_size(unsigned long) - hash range is 0..hash_size-1
+    """
+    
     def __cinit__(self, np.uint64_t hash_size):
+        """Stores given hash size."""
         if hash_size > MAX_HASH_SIZE:
             raise ValueError(
                 "We appreciate your hardware, but the maximum "
@@ -14,4 +20,5 @@ cdef class HashFunction(object):
         self.hash_size = hash_size
     
     cpdef np.uint64_t hash(self, str string):
+        """Applies hash to the string given."""
         return spooky.hash64(string) % self.hash_size
