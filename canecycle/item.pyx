9c22e87fbf38a3e1a9ae85aff91c727a87c72f5a
graring <shiryaev.petr@gmail.com>
2015-05-12 22:19:41 +0400

    item documentation

--- canecycle/item.pyx
+++ canecycle/item.pyx
@@ -1,6 +1,14 @@
 
 
 cdef class Item(object):
+    """Class to store a piece of data.
+    Attributes:
+        label(int) - item label, 1 or -1
+        weight(float) - item importance for optimizer
+        data(array) - item features, supposed to be sparse
+        indices(array) - indices of non-zero item features,
+            supposed to be sparse
+    """
     def __cinit__(self):
         self.label = 0
         self.weight = 1.   
